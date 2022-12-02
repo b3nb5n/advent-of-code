ec76a5432dc6b27790ce20f21db5ea1698c6c14f
Ben Baldwin <benbaldwin000@gmail.com>
2022-12-02 13:14:27 -0700

    test

--- 2022/day_01/src/main.rs
+++ 2022/day_01/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello world!")
+    let elves = vec![
+        vec![1000, 2000, 3000],
+        vec![4000],
+        vec![5000, 6000],
+        vec![7000, 8000, 9000],
+        vec![10000],
+    ];
+
+    let mut calories = Vec::<i32>::new();
+    for elf in elves.iter() {
+        let mut sum = 0;
+        for calorie_count in elf.iter() {
+            sum += *calorie_count;
+        }
+
+        calories.push(sum);
+    }
+
+    let mut max = 0;
+    for calorie_count in calories.iter() {
+        if max < *calorie_count {
+            max = *calorie_count
+        }
+    }
+
+    println!("max: {:?}", max)
 }
