87f4166a912942ea47de6cab6f23a91b43957767
Ben Baldwin <benbaldwin000@gmail.com>
2022-12-02 19:36:56 -0700

    part 2

--- 2022/day_01/src/main.rs
+++ 2022/day_01/src/main.rs
@@ -7,7 +7,7 @@ fn main() -> io::Result<()> {
     let file = File::open("./day_01/input_01.txt")?;
     let reader = io::BufReader::new(file);
 
-    let mut max = 0;
+    let mut calorie_counts = Vec::new();
     let mut current_elf = 0;
 
     for line_result in reader.lines() {
@@ -19,16 +19,17 @@ fn main() -> io::Result<()> {
         current_elf += match line.parse::<i32>() {
             Ok(x) => x,
             Err(_) => {
-                if current_elf > max {
-                    max = current_elf
-                }
-
+                calorie_counts.push(current_elf);
                 current_elf = 0;
                 continue;
-            },
+            }
         };
     }
 
-    println!("max calories: {}", max);
+    calorie_counts.sort();
+    println!(
+        "max calories: {:?}",
+        calorie_counts.iter().rev().take(3).sum::<i32>()
+    );
     Ok(())
 }
