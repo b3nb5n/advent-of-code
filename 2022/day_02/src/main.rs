bc2bd4eea3aa8528fcd660635827ad7329637365
Ben Baldwin <benbaldwin000@gmail.com>
2022-12-03 19:37:05 -0700

    day 2 part 1

--- 2022/day_02/src/main.rs
+++ 2022/day_02/src/main.rs
@@ -11,12 +11,12 @@ enum Move {
 }
 
 impl Move {
-    fn from_char(ch: &char) -> Result<Self, ()> {
+    fn from_char(ch: char) -> Result<Self, String> {
         match ch {
             'A' | 'X' => Ok(Self::Rock),
             'B' | 'Y' => Ok(Self::Paper),
             'C' | 'Z' => Ok(Self::Scissors),
-            _ => Err(()),
+            _ => Err(format!("unknow character: {}", ch).to_owned()),
         }
     }
 }
@@ -28,57 +28,50 @@ enum Outcome {
 }
 
 impl Outcome {
-    fn from_moves(mv: &Move, op_mv: &Move) -> Self {
-        if *mv == *op_mv {
-            return Self::Draw;
+    fn from_moves(mv: Move, op_mv: Move) -> Self {
+        // this implementation makes me feel very nooby
+        // rust hurts me so much and its only day 2
+        match mv {
+            Move::Rock => match op_mv {
+                Move::Rock => Outcome::Draw,
+                Move::Paper => Outcome::Lose,
+                Move::Scissors => Outcome::Win,
+            },
+            Move::Paper => match op_mv {
+                Move::Rock => Outcome::Win,
+                Move::Paper => Outcome::Draw,
+                Move::Scissors => Outcome::Lose,
+            },
+            Move::Scissors => match op_mv {
+                Move::Rock => Outcome::Lose,
+                Move::Paper => Outcome::Win,
+                Move::Scissors => Outcome::Draw,
+            },
         }
-        if *mv > *op_mv && !(*mv == Move::Rock && *op_mv == Move::Scissors) {
-            return Self::Win;
-        }
-
-        return Self::Lose;
     }
 }
 
-fn score_round(mv: &Move, op_mv: &Move) -> i32 {
-    (*mv as i32) + (Outcome::from_moves(mv, op_mv) as i32)
+fn score(mv: Move, op_mv: Move) -> i32 {
+    (mv as i32) + (Outcome::from_moves(mv, op_mv) as i32)
 }
 
 fn main() {
-    let file = fs::File::open("./day_01/input_01.txt").expect("error reading file");
+    let file = fs::File::open("./day_02/input_01.txt").expect("error reading file");
     let reader = io::BufReader::new(file);
 
-    let x = reader.lines().map(|line| { 
-        let mut chars = line.expect("error reading line").to_owned().chars();
-        score_round(
-            &Move::from_char(&chars.nth(0).expect("missing move")).expect("error parsing move"),
-            &Move::from_char(&chars.nth(0).expect("missing move")).expect("error parsing move"),
-        )
+    let score: i32 = reader
+        .lines()
+        .map(|read_result| {
+            let line = read_result.expect("error reading line");
+            let bytes = line.as_bytes();
+            let mv = Move::from_char(bytes[2] as char).expect("error parsing move");
+            let op_mv = Move::from_char(bytes[0] as char).expect("error parsing opponent move");
 
-    }).collect::<Vec<_>>();
+            score(mv, op_mv)
+        })
+        .collect::<Vec<_>>()
+        .iter()
+        .sum();
 
-    let mut score = 0;
-    for line in reader.lines() {
-        let chars = match line {
-            Ok(s) => s.to_owned().chars(),
-            Err(_) => panic!("Error reading line"),
-        };
-
-        let mv_ch = chars.nth(0).unwrap();
-        let mv = match Move::from_char(&mv_ch)  {
-            Ok(mv) => mv,
-            Err(_) => panic!("error parsing move: {}", mv_ch)
-        };
-
-        let op_mv_ch = chars.nth(2).unwrap();
-        let op_mv = match Move::from_char(&op_mv_ch)  {
-            Ok(mv) => mv,
-            Err(_) => panic!("error parsing move: {}", mv_ch)
-        };
-
-
-        score += score_round(&mv, &op_mv);
-    }
-
-    println!("score: {}", score);
+    println!("score: {}", score)
 }
